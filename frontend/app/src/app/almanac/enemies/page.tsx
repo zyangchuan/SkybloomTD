e4093119ddbb486abc1740ac8d099d414876c8b2
Wei En <gouyitiao666@gmail.com>
2026-07-16 21:26:55 +0800

    modified the set up of the almanac pages for both enemies and birds

--- frontend/app/src/app/almanac/enemies/page.tsx
+++ frontend/app/src/app/almanac/enemies/page.tsx
@@ -7,12 +7,12 @@ import ButtonWhite from "@/components/ButtonWhite";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
-import { BIRD_INFO, AlmanacBirdsEntry}  from "@/data/almanac";
+import { ENEMY_INFO, AlmanacEnemiesEntry}  from "@/data/almanac";
 
-export default function BirdsPage() {
+export default function EnemiesPage() {
 
     const route = useRouter();
-    const [selectedBird, setSelectedBird] = useState< AlmanacBirdsEntry>(BIRD_INFO[0] ?? null);
+    const [selectedEnemy, setSelectedEnemy] = useState< AlmanacEnemiesEntry>(ENEMY_INFO[0] ?? null);
 
     return (
         <div className="flex min-h-screen items-center justify-center">
@@ -29,36 +29,36 @@ export default function BirdsPage() {
                     </ButtonWhite>
                 </div>
 
-                <div className="absolute left-[60px] top-[100px] bottom-[20px] right-[60px]">
+                <div className="absolute left-[60px] top-[40px] bottom-[20px] right-[60px]">
                     {/* Main content*/}
                     <div className="grid h-full min-h-0 grid-cols-[480px_1fr]">
 
-                        {/* BIRDS */}
+                        {/* ENEMIES */}
                         <div className="border-r-4 flex flex-col min-h-0 border-orange-900/20">
                             <h1 className="mb-10 text-center font-bold text-[50px] text-zinc-300">
-                                Birds
+                                Enemies
                             </h1>
 
                             <div className="grid grid-cols-2 gap-10 min-h-0 justify-items-center overflow-y-auto pb-[20px]">
-                                 {/* Birds icon */}
-                                {BIRD_INFO.map((bird) => (
+                                 {/* Enemies icon */}
+                                {ENEMY_INFO.map((enemy) => (
                                         <button
-                                            key={bird.id}
-                                            onClick={() => setSelectedBird(bird)}
+                                            key={enemy.id}
+                                            onClick={() => setSelectedEnemy(enemy)}
                                             className="flex justify-items-center w-[200px] h-[220px]"
                                         >
                                             <BlueSquare className="flex flex-col items-center h-full w-full cursor-pointer">
                                                 <div className="relative h-full w-full">
                                                     <Image
-                                                        src={`/assets/birds/${bird.id}_head.png`}
-                                                        alt={`${bird.id}`}
+                                                        src={`/assets/enemies/${enemy.id}.png`}
+                                                        alt={`${enemy.id}`}
                                                         fill
                                                         className="object-contain"
                                                     />
                                                 </div>
 
                                                 <h1 className="text-[30px] whitespace-nowrap ">
-                                                    {bird.name}
+                                                    {enemy.name}
                                                 </h1>
 
                                             </BlueSquare>
@@ -69,42 +69,42 @@ export default function BirdsPage() {
                             </div>
                         </div>
 
-                        {/* Specific BIRD */}
+                        {/* Specific enemy */}
                         <div className="flex h-full min-h-0 flex-col items-center overflow-hidden pl-3">
                             <p className="font-bold mb-10 whitespace-nowrap text-[50px] text-zinc-300">
-                                Bird Details
+                                Enemy Details
                             </p>
 
                             <BlueSquare className="flex flex-col items-center h-54 w-50">
                                 <div className="relative h-full w-full">
                                     <Image
-                                        src={`/assets/birds/${selectedBird.id}_head.png`}
-                                        alt={`${selectedBird.id}`}
+                                        src={`/assets/enemies/${selectedEnemy.id}.png`}
+                                        alt={`${selectedEnemy.id}`}
                                         fill
                                         className="object-contain"
                                     />
                                 </div>
 
                                 <h1 className="text-[30px] whitespace-nowrap">
-                                    {selectedBird.name}
+                                    {selectedEnemy.name}
                                 </h1>
                             </BlueSquare>
 
 
                             <div className="mt-10 flex min-h-0 w-full flex-1 flex-col overflow-hidden gap-1">
                                 <div className="border-b-4 flex items-center justify-between border-orange-900/20 pb-2">
-                                    <span className="font-bold"> Damage:</span>
-                                    <span className="text-red-500"> {selectedBird.stats.damage} </span>
+                                    <span className="font-bold"> Health:</span>
+                                    <span className="text-red-500"> {selectedEnemy.stats.health} </span>
                                 </div>
 
 
                                 <div className="border-b-4 flex items-center justify-between border-orange-900/20 pb-2">
-                                    <span className="font-bold"> Attack Speed:</span>
-                                    <span className="text-red-500"> {selectedBird.stats.attack_speed} </span>
+                                    <span className="font-bold"> Movement:</span>
+                                    <span className="text-red-500"> {selectedEnemy.stats.movement} </span>
                                 </div>
 
                                 <div className="min-h-0 overflow-y-auto break-words pr-2  text-[18px] leading-relaxed">
-                                   {selectedBird.desc}
+                                   {selectedEnemy.desc}
                                 </div>
 
                             </div>
--- frontend/app/src/app/almanac/page.tsx
+++ frontend/app/src/app/almanac/page.tsx
@@ -1,68 +1,73 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import OrangeSquare from "@/components/OrangeSquare";
 import BlueSquare from "@/components/BlueSquare";
-
-type Category = "birds" | "enemies";
+import ButtonWhite from "@/components/ButtonWhite";
+import { ArrowLeft } from "lucide-react";
 
 export default function AlmanacPage() {
 
     const route = useRouter();
 
-    const [selectedCategory, setSelectedCategory] =
-        useState<Category | null>(null);
-
     return (
         <div className="flex min-h-screen items-center justify-center">
             <OrangeSquare className="relative h-[800px] w-[1000px]">
-                <h1 className="absolute left-1/2 top-12 -translate-x-1/2 text-[48px] font-bold">
+                 <div className="absolute translate-x-[10px]">
+                    {/* Back Button */}
+                    <ButtonWhite
+                        onClick={() => route.back()}
+                        className="flex w-[166px] h-[50px] items-center justify-center gap-2"
+                    >
+                        <ArrowLeft className="w-[30px] h-[60px]" />
+                       <span className="text-[23px]">Back</span>
+                    </ButtonWhite>
+                </div>
+
+                <h1 className="absolute left-1/2 top-12 -translate-x-1/2 text-[48px] font-bold text-amber-300">
                     SkyBloomTD Almanac
                 </h1>
 
-                {selectedCategory === null && (
-                    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[100px]">
-                        <div className="flex gap-[100px]">
-                            <button
-                                onClick={() => route.push('/almanac/birds')}
-                                className="relative flex h-[300px] w-[400px] items-center"
-                            >
-                                <BlueSquare className="relative h-full w-full">
-                                    <Image
-                                        src="/assets/birds/sparrow_head.png"
-                                        fill
-                                        alt="birds"
-                                        className="-translate-y-[40px] scale-[0.6] object-contain"
-                                    />
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[100px]">
+                    <div className="flex gap-[100px]">
+                        <button
+                            onClick={() => route.push('/almanac/birds')}
+                            className="relative flex h-[300px] w-[400px] items-center"
+                        >
+                            <BlueSquare className="relative h-full w-full cursor-pointer">
+                                <Image
+                                    src="/assets/birds/sparrow_head.png"
+                                    fill
+                                    alt="birds"
+                                    className="-translate-y-[40px] scale-[0.6] object-contain"
+                                />
 
-                                    <p className="absolute left-1/2 -translate-x-1/2 translate-y-[180px] text-[42px] font-bold">
-                                        BIRDS
-                                    </p>
-                                </BlueSquare>
-                            </button>
+                                <p className="absolute left-1/2 -translate-x-1/2 translate-y-[180px] text-[42px] font-bold">
+                                    BIRDS
+                                </p>
+                            </BlueSquare>
+                        </button>
 
-                            <button
-                                onClick={() => setSelectedCategory("enemies")}
-                                className="flex h-[300px] w-[400px] items-center"
-                            >
-                                <BlueSquare className="relative h-full w-full">
-                                    <Image
-                                        src="/assets/enemies/smog.png"
-                                        fill
-                                        alt="enemies"
-                                        className="-translate-y-[40px] scale-[0.7] object-contain"
-                                    />
+                        <button
+                            onClick={() => route.push("/almanac/enemies")}
+                            className="flex h-[300px] w-[400px] items-center"
+                        >
+                            <BlueSquare className="relative h-full w-full cursor-pointer">
+                                <Image
+                                    src="/assets/enemies/smog.png"
+                                    fill
+                                    alt="enemies"
+                                    className="-translate-y-[40px] scale-[0.7] object-contain"
+                                />
 
-                                    <p className="absolute left-1/2 -translate-x-1/2 translate-y-[180px] text-[42px] font-bold">
-                                        ENEMIES
-                                    </p>
-                                </BlueSquare>
-                            </button>
-                        </div>
+                                <p className="absolute left-1/2 -translate-x-1/2 translate-y-[180px] text-[42px] font-bold">
+                                    ENEMIES
+                                </p>
+                            </BlueSquare>
+                        </button>
                     </div>
-                )}
+                </div>
             </OrangeSquare>
         </div>
     );
